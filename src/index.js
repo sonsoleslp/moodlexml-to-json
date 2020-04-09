4ba8b8266fa48823661202c1c2946237010eeb3d
sonsoleslp <poynting.slp@gmail.com>
2020-04-09 17:06:29 +0200

    Small fix

--- src/index.js
+++ src/index.js
@@ -76,95 +76,113 @@ const parseSubquestions = (sub) => {
   return sub;
 };
 
-const parseQuestion = (q) => {
-  let answers;
-  let correctAnswer;
-  if (!q || !q.$) {
-    return {};
+const parseQuestion = (q, index) => {
+  try {
+    let answers;
+    let correctAnswer;
+    if (!q || !q.$) {
+      console.warn(`Question ${index} is missing`);
+      return null;
+    }
+    const { type } = q.$;
+
+    if (type === 'category') {
+      return { type, category: parseHTMLtext(q.category) };
+    }
+    const question = {
+      type,
+      name: parseHTMLtext(q.name),
+      questiontext: parseHTMLtext(q.questiontext),
+      format: (q.questiontext &&
+        q.questiontext &&
+        q.questiontext[0] &&
+        q.questiontext[0].$ &&
+        q.questiontext[0].$.format) ? q.questiontext[0].$.format : undefined,
+      penalty: parseText(q.penalty),
+      hidden: parseText(q.hidden),
+      generalfeedback: parseText(q.generalfeedback),
+      defaultgrade: parseText(q.defaultgrade),
+      tags: parseTags(q.tags),
+      img: parseImage(q.image)
+    };
+    switch (type) {
+      case 'multichoice':
+        answers = parseAnswers(type, q.answer);
+        correctAnswer = answers.map((a, i) => ((a.score !== 0) ? i : null)).filter(a => a !== null);
+        question.answers = answers;
+        question.correctAnswer = correctAnswer;
+        question.single = parseText(q.single) || false;
+        question.shuffleanswers = q.shuffleanswers === 1 || q.shuffleanswers === '1';
+        question.correctfeedback = parseText(q.correctfeedback);
+        question.partiallycorrectfeedback = parseText(q.partiallycorrectfeedback);
+        question.incorrectfeedback = parseText(q.incorrectfeedback);
+        question.answernumbering = parseText(q.answernumbering);
+        break;
+
+      case 'truefalse':
+        answers = parseAnswers(type, q.answer);
+        [correctAnswer] = answers
+          .map(a => ((a.score !== 0) ? a.text : null))
+          .filter(a => a !== null);
+        try {
+          correctAnswer = JSON.parse(correctAnswer);
+        } catch (e) {
+          // Do nothing
+        }
+        question.answers = answers;
+        question.correctAnswer = correctAnswer;
+        break;
+
+      case 'numerical':
+        answers = parseAnswers(type, q.answer);
+        correctAnswer = answers
+          .map(a => ((a.score !== 0) ? tryParse(a.text) : null))
+          .filter(a => a !== null);
+        question.answers = correctAnswer;
+        question.correctAnswer = correctAnswer;
+        question.tolerance = parseText(q.tolerance);
+        break;
+
+      case 'matching':
+        /*
+        let answers = parseAnswers(type, q.answer);
+        let correctAnswer = answers.map((a,i) =>
+          (a.score !== 0)? i: null).filter(a => a!== null)
+        question.answers = answers;
+        question.correctAnswer = correctAnswer; */
+        question.answers = parseSubquestions(q.subquestion);
+        question.correctAnswer = question.answers.map(a => [a.text, a.answer]);
+        break;
+
+      case 'essay':
+        break;
+
+      case 'shortanswer':
+        answers = parseAnswers(type, q.answer);
+        correctAnswer = answers.map((a, i) => ((a.score !== 0) ? i : null))
+          .filter(a => a !== null);
+        question.answers = answers;
+        break;
+      case 'cloze':
+        throw new Error('Cloze question not supported');
+      case 'order':
+        answers = parseSubquestions(q.subquestion);
+        question.answers = answers.map(a => a.text);
+        question.correctAnswer = answers.map(a => a.answer);
+        question.shuffleanswers = q.shuffleanswers === 1 || q.shuffleanswers === '1';
+        console.log(question);
+        break;
+      case 'description':
+        break;
+      default:
+        throw new Error('Unknown exercise type');
+    }
+    return question;
+  } catch (error) {
+    console.warn(`Could not parse question ${index + 1} (${JSON.stringify(q)})
+    Error: ${error}`);
+    return null;
   }
-  const { type } = q.$;
-
-  if (type === 'category') {
-    return { type, category: parseHTMLtext(q.category) };
-  }
-  const question = {
-    type,
-    name: parseHTMLtext(q.name),
-    questiontext: parseHTMLtext(q.questiontext),
-    format: (q.questiontext &&
-      q.questiontext &&
-      q.questiontext[0] &&
-      q.questiontext[0].$ &&
-      q.questiontext[0].$.format) ? q.questiontext[0].$.format : undefined,
-    penalty: parseText(q.penalty),
-    hidden: parseText(q.hidden),
-    generalfeedback: parseText(q.generalfeedback),
-    defaultgrade: parseText(q.defaultgrade),
-    tags: parseTags(q.tags),
-    img: parseImage(q.image)
-  };
-  switch (type) {
-    case 'multichoice':
-      answers = parseAnswers(type, q.answer);
-      correctAnswer = answers.map((a, i) => ((a.score !== 0) ? i : null)).filter(a => a !== null);
-      question.answers = answers;
-      question.correctAnswer = correctAnswer;
-      question.single = parseText(q.single) || false;
-      question.shuffleanswers = q.shuffleanswers === 1 || q.shuffleanswers === '1';
-      question.correctfeedback = parseText(q.correctfeedback);
-      question.partiallycorrectfeedback = parseText(q.partiallycorrectfeedback);
-      question.incorrectfeedback = parseText(q.incorrectfeedback);
-      question.answernumbering = parseText(q.answernumbering);
-      break;
-
-    case 'truefalse':
-      answers = parseAnswers(type, q.answer);
-      [correctAnswer] = answers
-        .map(a => ((a.score !== 0) ? a.text : null))
-        .filter(a => a !== null);
-      try {
-        correctAnswer = JSON.parse(correctAnswer);
-      } catch (e) {
-        // Do nothing
-      }
-      question.answers = answers;
-      question.correctAnswer = correctAnswer;
-      break;
-
-    case 'numerical':
-      answers = parseAnswers(type, q.answer);
-      correctAnswer = answers
-        .map(a => ((a.score !== 0) ? tryParse(a.text) : null))
-        .filter(a => a !== null);
-      question.answers = correctAnswer;
-      question.correctAnswer = correctAnswer;
-      question.tolerance = parseText(q.tolerance);
-      break;
-
-    case 'matching':
-      /*
-      let answers = parseAnswers(type, q.answer);
-      let correctAnswer = answers.map((a,i) =>
-        (a.score !== 0)? i: null).filter(a => a!== null)
-      question.answers = answers;
-      question.correctAnswer = correctAnswer; */
-      question.answers = parseSubquestions(q.subquestion);
-      question.correctAnswer = question.answers.map(a => [a.text, a.answer]);
-      break;
-
-    case 'essay':
-      break;
-
-    case 'shortanswer':
-      answers = parseAnswers(type, q.answer);
-      correctAnswer = answers.map((a, i) => ((a.score !== 0) ? i : null)).filter(a => a !== null);
-      question.answers = answers;
-      break;
-
-    default:
-      throw new Error('Unknown exercise type');
-  }
-  return question;
 };
 
 const moodleXMLtoJSON = (xmlStr, callback) => {
@@ -174,7 +192,7 @@ const moodleXMLtoJSON = (xmlStr, callback) => {
       return;
     }
     try {
-      const questions = result.quiz.question.map(q => parseQuestion(q));
+      const questions = result.quiz.question.map((q, i) => parseQuestion(q, i)).filter(Boolean);
       callback({ questions }, undefined);
     } catch (e) {
       callback(undefined, e);
--- test/index.js
+++ test/index.js
@@ -2,7 +2,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const normalPath = path.normalize(`${__dirname}/test2.xml`);
+const normalPath = path.normalize(`${__dirname}/test.xml`);
 const contents = fs.readFileSync(normalPath, 'utf8');
 const moodleXMLtoJSON = require('../src');
 const assert = require("chai").assert;
@@ -11,69 +11,69 @@ const assert = require("chai").assert;
 const createCallback = (assertFun) => {
   return (res,err) => {
     let questions = (res && res.questions) ? res.questions : res;
-    assertFun(questions,err);
+    assertFun(questions, err);
   }
 }
 
 describe('General XML to JSON', () => {
   it('should parse', () => {
-    moodleXMLtoJSON(contents, createCallback((res,err)=> assert(!!res, true)));
+    moodleXMLtoJSON(contents, createCallback((res, err)=> assert(!!res, true)));
   });
 
   it('should not parse', () => {
-    moodleXMLtoJSON("WRONG XML", createCallback((res,err)=> assert(!!err, true)));
+    moodleXMLtoJSON("WRONG XML", createCallback((res, err)=> assert(!!err, true)));
   });
 
   it('should not have errors', () => {
-    moodleXMLtoJSON(contents, createCallback((res,err)=> assert(err,undefined)));
+    moodleXMLtoJSON(contents, createCallback((res, err)=> assert(err,undefined)));
   });
 
   it('should not recognize exercise type', () => {
-    moodleXMLtoJSON(contents.replace("numerical","unknown")
-      , createCallback((res,err)=> assert(!!err,true)))
+    moodleXMLtoJSON(contents.replace("numerical", "unknown")
+      , createCallback((res, err)=> assert(!!err, true)))
  
   });
 });
 
 describe('Category', () => {
   it('should contain category', () => {
-    moodleXMLtoJSON(contents, createCallback((res,err)=> assert(res[0].type,"category")));
+    moodleXMLtoJSON(contents, createCallback((res, err)=> assert(res[0].type, "category")));
   });
 });
 
 describe('Truefalse exercise', () => {
   it('should contain truefalse', () => {
-    moodleXMLtoJSON(contents, createCallback((res,err)=> assert(res[1].type,"truefalse")));
+    moodleXMLtoJSON(contents, createCallback((res, err)=> assert(res[1].type, "truefalse")));
   });
 });
 
 describe('Multiple choice exercise', () => {
 it('should contain multichoice', () => {
-    moodleXMLtoJSON(contents, createCallback((res,err)=> assert(res[4].type,"multichoice")));
+    moodleXMLtoJSON(contents, createCallback((res, err)=> assert(res[4].type, "multichoice")));
   });
 });
 
 describe('Short answer exercise', () => {
 it('should contain shortanswer', () => {
-    moodleXMLtoJSON(contents, createCallback((res,err)=> assert(res[11].type,"shortanswer")));
+    moodleXMLtoJSON(contents, createCallback((res, err)=> assert(res[11].type, "shortanswer")));
   });
 });
 
 describe('Matching exercise', () => {
   it('should contain matching', () => {
-    moodleXMLtoJSON(contents, createCallback((res,err)=> assert(res[15].type,"matching")));
+    moodleXMLtoJSON(contents, createCallback((res, err)=> assert(res[15].type, "matching")));
   });
 });
  
 describe('Numerical exercise', () => {
   it('should contain numerical', () => {
-    moodleXMLtoJSON(contents, createCallback((res,err)=> assert(res[16].type,"numerical")));
+    moodleXMLtoJSON(contents, createCallback((res, err)=> assert(res[16].type, "numerical")));
   });
 });
   
 describe('Essay exercise', () => {
   it('should contain essay', () => {
-    moodleXMLtoJSON(contents, createCallback((res,err)=> assert(res[17].type,"essay")));
+    moodleXMLtoJSON(contents, createCallback((res, err)=> assert(res[17].type, "essay")));
   });
 }); 
 
